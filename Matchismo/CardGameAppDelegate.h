d698f41b8803ce1f23f1f55e11ee1dbed2d2835a
benburton <ben@neo.com>
2013-04-03 10:32:10 -0400

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Ben Burton on 4/3/13.
+//  Copyright (c) 2013 Ben Burton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Ben Burton on 4/3/13.
+//  Copyright (c) 2013 Ben Burton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}

850c27d7ffc21fe99e9c78d22cc31982d347a0f8
benburton <ben@neo.com>
2013-04-04 22:04:20 -0400

    Add sorting to scores controller

--- Matchismo/GameResult.h
+++ Matchismo/GameResult.h
@@ -1,13 +1,18 @@
-//
-//  GameResult.h
-//  Matchismo
-//
-//  Created by Ben Burton on 4/4/13.
-//  Copyright (c) 2013 Ben Burton. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface GameResult : NSObject
+
++ (NSArray *)allGameResults; // of GameResult
+
+@property (readonly, nonatomic) NSDate *start;
+@property (readonly, nonatomic) NSDate *end;
+@property (readonly, nonatomic) NSTimeInterval duration;
+@property (nonatomic) int score;
+
+#pragma mark - Sorting
+
+- (NSComparisonResult)compareScoreToGameResult:(GameResult *)otherResult;
+- (NSComparisonResult)compareEndDateToGameResult:(GameResult *)otherResult;
+- (NSComparisonResult)compareDurationToGameResult:(GameResult *)otherResult;
 
 @end
--- Matchismo/GameResultViewController.m
+++ Matchismo/GameResultViewController.m
@@ -1,38 +1,84 @@
-//
-//  GameResultViewController.m
-//  Matchismo
-//
-//  Created by Ben Burton on 4/4/13.
-//  Copyright (c) 2013 Ben Burton. All rights reserved.
-//
-
 #import "GameResultViewController.h"
+#import "GameResult.h"
 
 @interface GameResultViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextView *display;
+@property (nonatomic) SEL sortSelector;
 @end
 
 @implementation GameResultViewController
 
+- (void)updateUI
+{
+    NSString *displayText = @"";
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle:NSDateFormatterShortStyle];
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    for (GameResult *result in [[GameResult allGameResults] sortedArrayUsingSelector:self.sortSelector]) {
+        displayText = [displayText stringByAppendingFormat:@"Score: %d (%@, %0g)\n", result.score, [formatter stringFromDate:result.end], round(result.duration)];
+    }
+    self.display.text = displayText;
+}
+
+#pragma mark - View Controller Lifecycle
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self updateUI];
+}
+
+#pragma mark - Sorting
+
+@synthesize sortSelector = _sortSelector;
+
+- (SEL)sortSelector
+{
+    if (!_sortSelector) {
+        _sortSelector = @selector(compareScoreToGameResult:);
+    }
+    return _sortSelector;
+}
+
+- (void)setSortSelector:(SEL)sortSelector
+{
+    _sortSelector = sortSelector;
+    [self updateUI];
+}
+
+- (IBAction)sortByDate
+{
+    self.sortSelector = @selector(compareEndDateToGameResult:);
+}
+
+- (IBAction)sortByScore
+{
+    self.sortSelector = @selector(compareScoreToGameResult:);
+}
+
+- (IBAction)sortByDuration
+{
+    self.sortSelector = @selector(compareDurationToGameResult:);
+}
+
+
+#pragma mark - (Unused) Initialization before viewDidLoad
+
+- (void)setup
+{
+    // initialization that can't wait until viewDidLoad
+}
+
+- (void)awakeFromNib
+{
+    [self setup];
+}
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
+    [self setup];
     return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
